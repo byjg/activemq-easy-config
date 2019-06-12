d65b855c29bf25e20b9d3561caf3ef597383ed91
Joao Gilberto Magalhaes <joao@byjg.com.br>
2019-06-12 00:57:44 -0500

    Added Setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='aec',
+    version='0.1.1',
+    scripts=['aec'],
+    author="Joao Gilberto Magalhaes",
+    author_email="joao@byjg.com.br",
+    description="ActiveMQ Easy Config",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/byjg/activemq-easy-config",
+    packages=setuptools.find_packages(),
+    install_requires=open("requirements.txt", 'r').readlines(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+ )
