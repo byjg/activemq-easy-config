48c2366ff11dfdbc63dd9dc1e06412743d521144
Joao Gilberto Magalhaes <joao@byjg.com.br>
2019-06-12 22:34:22 -0500

    Fix PyYaml deprecation warning

--- parseyml/__init__.py
+++ parseyml/__init__.py
@@ -6,7 +6,7 @@ class Yaml:
 
     def __init__(self, filename, xmlfilename):
         with open(filename, 'r') as content_file:
-            self.config_yaml = yaml.load(content_file.read())
+            self.config_yaml = yaml.load(content_file.read(), Loader=yaml.FullLoader)
         # print(self.config_yaml)
         self.xmlfilename = xmlfilename
 
